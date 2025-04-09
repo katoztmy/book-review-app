e4d8b6cc2748e50c9fb49837569a330dbb421306
katoztmy <kato.eng.dao@gmail.com>
2025-04-09 20:52:29 +0900

    fix RTL test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 import App from "../src/Test";
 
 describe("App", () => {
+  // React Testing LibraryはRTLと訳されるらしい
+  // RTLのrender関数による出力が不明なときは screen.debug()を使う
   test("renders App component", () => {
     render(<App />);
-    screen.debug();
+    expect(screen.getByPlaceholderText("メールアドレス")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("パスワード")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "サインイン" })
+    ).toBeInTheDocument();
   });
 });
